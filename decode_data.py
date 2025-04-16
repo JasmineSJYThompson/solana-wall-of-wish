9ede59ce07ab020c31bce1fd7e5074dbae9497d3
Jasmine <jasmine.s.jy.thompson@gmail.com>
2025-04-16 13:03:14 +0100

    Added a way to decode the data written to the wish account in the anchor test

--- decode_data.py
+++ decode_data.py
@@ -1,7 +1,26 @@
+import requests
+import json
+
+url = "https://api.devnet.solana.com"
+
+pda = "AXG5yPrycHiZ5hzEGebRjge6HsdGArpkL7uyrwok6vnJ"
+
+payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "getAccountInfo",
+    "params": [pda]
+}
+
+response = requests.post(url, json=payload)
+response_json = response.json()
+
 import base58
 from struct import unpack
 
-b58_data = "EJLbdAgd22fDcpovK3woGAJTv5q3xDPvyiWspiynDRSz78wVp2C42cPooLhC6T1QkLvmdSLBK1xEZcBBEqsDd8QaK715h"
+#b58_data = "EJLbdAgd22fDcpovK3woGAJTv5q3xDPvyiWspiynDRSz78wVp2C42cPooLhC6T1QkLvmdSLBK1xEZcBBEqsDd8QaK715h"
+b58_data = response_json["result"]["value"]["data"]
+
 raw = base58.b58decode(b58_data)
 
 # Skip 8-byte discriminator
